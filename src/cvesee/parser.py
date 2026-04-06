4a9e5a79ee128b7f5834df90a0ab6e1d782c1aad
Matt Box <mdbox037a@protonmail.com>
2026-04-05 21:54:42 -0400

    begin work on parsing functions, but then move to better data definition modeling

---
+++ src/cvesee/parser.py
@@ -0,0 +1,11 @@
+from models import NVDInfo, CVE
+
+
+def create_cve_parent_class(nvd_data: dict) -> CVE:
+    """set up parent CVE data class with basic info"""
+
+    pass
+
+
+def parse_nvd_cve_data(nvd_data: dict) -> CVE:
+    pass
