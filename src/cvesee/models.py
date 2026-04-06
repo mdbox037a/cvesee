2912456866ab3a92af5e4ef09b2037612b7c752f
Matt Box <mdbox037a@protonmail.com>
2026-04-05 21:55:19 -0400

    begin pydantic dataclass implementation to prep parsing of nested NVD API data

--- src/cvesee/models.py
+++ src/cvesee/models.py
@@ -1,5 +1,7 @@
+from pydantic import BaseModel, Field
 from dataclasses import dataclass
-from typing import Optional
+from datetime import datetime
+from typing import List, Any, Optional
 
 
 @dataclass
@@ -23,3 +25,32 @@ class CVE:
     cve_id: str
     description: str
     nvd: NVDInfo
+
+
+class CVSS40Metric(BaseModel):
+    # bookmark
+    pass
+
+
+class NVDMetrics(BaseModel):
+    cvss_v40: Optional[List[CVSS40Metric]] = Field(None, alias="cvssMetricV40")
+    cvss_v31: Optional[List[CVSS31Metric]] = Field(None, alias="cvssMetricV31")
+    cvss_v30: Optional[List[CVSS30Metric]] = Field(None, alias="cvssMetricV30")
+    cvss_v2: Optional[List[CVSS2Metric]] = Field(None, alias="cvssMetricV2")
+
+
+class NVDCVEInfo(BaseModel):
+    id: str
+    cna: str = Field(alias="sourceIdentifier")
+    date_published: datetime = Field(alias="published")
+    date_last_modified: datetime = Field(alias="lastModified")
+    metrics: Optional[NVDMetrics] = None
+
+
+class NVDCVEWrapper(BaseModel):
+    cve: NVDCVEInfo
+
+
+class NVDAPIFull(BaseModel):
+    accessed: str = Field(alias="timestamp")
+    vulnerabilities: List[NVDCVEWrapper]
