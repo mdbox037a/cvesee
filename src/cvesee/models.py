0e2b2ad885adc6f16a7e8d5e5a75ae59ca863add
Matt Box <mdbox037a@protonmail.com>
2026-04-12 15:37:14 -0400

    complete validator logic for flattened NVD data

--- src/cvesee/models.py
+++ src/cvesee/models.py
@@ -21,8 +21,26 @@ class NVDInfo(BaseModel):
 
     @model_validator(mode="before")
     @classmethod
-    def flatten(cls, nvd_data: Any) -> Any:
-        c_wrap = nvd_data.get("vulnerabilities", [{}]).get("cve", {})
+    def flatten(cls, nvd_data: dict) -> dict:
+        """
+        ingest NVD API JSON output and return flattened data of interest for pydantic to
+        use to populate NVDInfo class fields
+        """
+
+        # initialize dict to return to pydantic (only required keys or keys required
+        # to avoid breaks in parsing logic below)
+        flat_data = {
+            "cve_id": "",
+            "description": "",
+            "date_published": "",
+            "date_last_modified": "",
+            "date_accessed": "",
+            "vendor_advisories": [],
+            "patches": [],
+            "reporting_cna": None,
+        }
+
+        c_wrap = nvd_data.get("vulnerabilities", [{}])[0].get("cve", {})
         m_wrap = c_wrap.get("metrics", {})
         # set cvss version preference order
         versions = [
@@ -38,14 +56,14 @@ class NVDInfo(BaseModel):
                 for index, item in enumerate(m_wrap[m_ver]):
                     cvss_data = m_wrap[m_ver][index].get("cvssData", {})
                     if item["source"] == "nist@nist.gov":
-                        nvd_data["nist_cna"] = True
-                        nvd_data["nist_score"] = cvss_data.get("baseScore")
-                        nvd_data["nist_severity"] = cvss_data.get("baseSeverity")
-                    elif not nvd_data["reporting_cna"]:
+                        flat_data["nist_cna"] = True
+                        flat_data["nist_score"] = cvss_data.get("baseScore")
+                        flat_data["nist_severity"] = cvss_data.get("baseSeverity")
+                    elif not flat_data.get("reporting_cna"):
                         # if we already have a reporting cna, skip
-                        nvd_data["reporting_cna"] = item["source"]
-                        nvd_data["rcna_score"] = cvss_data.get("baseScore")
-                        nvd_data["cna_severity"] = cvss_data.get("baseSeverity")
+                        flat_data["reporting_cna"] = item["source"]
+                        flat_data["rcna_score"] = cvss_data.get("baseScore")
+                        flat_data["cna_severity"] = cvss_data.get("baseSeverity")
 
                 # once we've captured one set of CVSS data, break, since we
                 # are moving in order from v4.0 -> 3.1 -> 3.0 -> 2
@@ -55,7 +73,7 @@ class NVDInfo(BaseModel):
         descriptions = c_wrap.get("descriptions", [])
         for desc in descriptions:
             if desc["lang"] == "en":
-                nvd_data["description"] = desc["value"]
+                flat_data["description"] = desc["value"]
 
                 # once we've captured the english string, break
                 break
@@ -63,14 +81,17 @@ class NVDInfo(BaseModel):
         # get vendor advisories and references
         references = c_wrap.get("references", [])
         for ref in references:
-            if "Vendor Advisory" in ref["tags"]:
-                nvd_data["vendor_advisories"].append(ref["url"])
+            tags = ref.get("tags", [])
+            if "Vendor Advisory" in tags:
+                flat_data["vendor_advisories"].append(ref["url"])
             if "Patch" in ref["tags"]:
-                nvd_data["patches"].append(ref["url"])
+                flat_data["patches"].append(ref["url"])
 
         # fill out the rest of the fields from the 'cve' wrapper
-        nvd_data["cve_id"] = c_wrap.get("id")
-        nvd_data["date_published"] = c_wrap.get("published")
-        nvd_data["date_last_modified"] = c_wrap.get("lastModified")
-        nvd_data["date_accessed"] = nvd_data["timestamp"]
-        nvd_data["cve_tags"] = c_wrap.get("cve_tags", [])
+        flat_data["cve_id"] = c_wrap.get("id")
+        flat_data["date_published"] = c_wrap.get("published")
+        flat_data["date_last_modified"] = c_wrap.get("lastModified")
+        flat_data["date_accessed"] = nvd_data["timestamp"]
+        flat_data["cve_tags"] = c_wrap.get("cve_tags", [])
+
+        return flat_data
