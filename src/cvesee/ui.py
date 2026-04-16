72425de2ad212dcf4e492cfd4fbee95032bb2ef3
Matt Box <mdbox037a@protonmail.com>
2026-04-15 23:07:02 -0400

    refine output to remove list brackets and add severity colors

--- src/cvesee/ui.py
+++ src/cvesee/ui.py
@@ -3,6 +3,14 @@ from rich import box
 from rich.console import Console
 
 
+SEVERITY_COLORS = {
+    "CRITICAL": "[dark_red]CRITICAL[/dark_red]",
+    "HIGH": "[red]HIGH[/red]",
+    "MEDIUM": "[magenta]MEDIUM[/magenta]",
+    "LOW": "[blue]LOW[/blue]",
+}
+
+
 def display_cve_summary(parsed_cve_data: dict, source: str) -> None:
     """use rich to format parsed NVD CVE info and send to console"""
     console = Console()
@@ -14,9 +22,16 @@ def display_cve_summary(parsed_cve_data: dict, source: str) -> None:
     )
     table.add_column(justify="right", no_wrap=True)
     table.add_column(justify="left")
-    for element, value in parsed_cve_data.model_dump().items():
-        table.add_row(element.replace("_", " ").title(), str(value))
-    # TODO: figure out how to print HttpUrls as strings
-    # TODO: color output for severities?
+    for element, value in parsed_cve_data.model_dump(mode="json").items():
+        match value:
+            case list():
+                pretty_value = "\n".join(value) if value else "[dim]None[/dim]"
+            case None:
+                pretty_value = f"[dim]{value}[/dim]"
+            case _:
+                pretty_value = str(value)
+        if pretty_value in SEVERITY_COLORS:
+            pretty_value = SEVERITY_COLORS[pretty_value]
+        table.add_row(element.replace("_", " ").title(), pretty_value)
 
     console.print(table)
