0233fb8b82ea54369e006e7e5fa638205c4b6e26
Matt Box <mdbox037a@protonmail.com>
2026-04-19 21:58:51 -0400

    implement api call to get Ubuntu security API

--- src/cvesee/api.py
+++ src/cvesee/api.py
@@ -2,14 +2,14 @@ import requests
 from cvesee import __version__
 
 
+headers = {"user-agent": f"cvesee/{__version__} (https://github.com/mdbox037a/cvesee)"}
+
+
 def fetch_nvd_cve_data(cve_id: str) -> dict | None:
     """get CVE data from NVD API and return JSON as dictionary"""
 
     nvd_api_url = "https://services.nvd.nist.gov/rest/json/cves/2.0"
     params = {"cveId": cve_id}
-    headers = {
-        "user-agent": f"cvesee/{__version__} (https://github.com/mdbox037a/cvesee)"
-    }
 
     try:
         nvd_data = requests.get(nvd_api_url, params=params, headers=headers, timeout=10)
@@ -20,4 +20,24 @@ def fetch_nvd_cve_data(cve_id: str) -> dict | None:
         return None
     except requests.Timeout:
         print("Request to NVD API timed out; please try again later")
+        return None
+
+
+def fetch_ubusecapi_cve_data(cve_id: str) -> dict | None:
+    """get CVE data from Ubuntu security API and return JSON as dictionary"""
+
+    ubusec_api_url = "https://ubuntu.com/security/cves"
+    params = {"cve_id": cve_id}
+
+    try:
+        ubusec_data = requests.get(
+            ubusec_api_url, params=params, headers=headers, timeout=10
+        )
+        ubusec_data.raise_for_status()
+        return ubusec_data.json()
+    except requests.RequestException as e:
+        print(f"Error requesting info for {cve_id}: {e}")
+        return None
+    except requests.Timeout:
+        print("Request to Ubuntu security API timed out; please try again later")
         return None
